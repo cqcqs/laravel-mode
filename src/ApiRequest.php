1efad0b023bb6d4983054e2705722415ea94d8c7
unknown <775038529@qq.com>
2020-08-12 15:55:20 +0800

    ApiRequest

---
+++ src/ApiRequest.php
@@ -0,0 +1,42 @@
+<?php
+namespace Cqcqs\Mode;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class ApiRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'code' => 10000,
+            'msg'  => $validator->errors()->first(),
+            //'errors' => (new ValidationException($validator))->errors()
+        ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+    
+}
